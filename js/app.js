b25e9d3215e23ea60acb188ae2f22a0bf69d9dfb
Larsdub <larsjeremy@hotmail.com>
2016-11-09 10:19:31 +0100

    Added OS Generated files to .gitignore

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-var d = new Date();
 var app = angular.module("workitApp", ["firebase"]);
 app.controller("workitCtrl", function($scope, $firebaseArray) {
+  //Connect to database.
   var ref = firebase.database().ref().child("work");
   // Create synced array
   $scope.opdrachten = $firebaseArray(ref);
@@ -11,14 +11,16 @@ app.controller("workitCtrl", function($scope, $firebaseArray) {
     }
   ]);
 
+  // Simple hack to reload page.
+  // Needed after login, need to fix.
   $scope.reloadPage = function(){
     location.reload();
   };
 
   $scope.login = function () {
+    // Sign in with email.
        firebase.auth().signInWithEmailAndPassword($scope.user.email, $scope.user.password)
          .then(function (response) {
-           console.log(response.email);
            $scope.userEmail = response.email;
            alert('Logged in.');
          }).catch(function (error) {
@@ -39,10 +41,12 @@ app.controller("workitCtrl", function($scope, $firebaseArray) {
     });
   };
 
+  // Check if user is logged in.
   firebase.auth().onAuthStateChanged(function(firebaseUser) {
     $scope.loggedUser = firebaseUser;
   });
 
+  // Add task, sync to firebase.
   $scope.addTask = function() {
     $scope.opdrachten.$add({
       vak: $scope.vak,
